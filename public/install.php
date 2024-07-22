436d1ceadc4c0ef501d05cf83fd6ff5604cc4b52
guiyumin <guiyumin@protonmail.com>
2024-07-21 19:19:00 -0700

    fix: part3: stepper

--- public/install.php
+++ public/install.php
@@ -22,6 +22,7 @@ $dotenv_file = PANDA_ROOT . '/.env';
 $writer = new \MirazMac\DotEnv\Writer($dotenv_file);
 
 
+
 ?>
 
 <!DOCTYPE html>
@@ -79,53 +80,78 @@ $writer = new \MirazMac\DotEnv\Writer($dotenv_file);
                 </div>
             </div>
             <div class="bs-stepper-content container">
-                <form onSubmit="return false">
-                    <div id="test-vl-1" role="tabpanel" class="bs-stepper-pane fade" aria-labelledby="stepper4trigger1">
-                        <h2>Choose Database</h2>
-                        <div class="form-group row mt-4">
-                            <label for="mongodb" class="col-3">MongoDB</label>
-                            <input type="checkbox" class="form-control col-3" id="mongodb" checked>
-                        </div>
-                        <div class="form-group row">
-                            <label for="mysql" class="col-3">MySQL</label>
-                            <input type="checkbox" class="form-control col-3" id="mysql" disabled>
-                        </div>
-                        <button class="btn btn-primary" onclick="stepper.next()">Next</button>
+
+                <div id="test-vl-1" role="tabpanel" class="bs-stepper-pane fade" aria-labelledby="stepper4trigger1">
+                    <h2>Choose Database</h2>
+                    <div class="form-group row mt-4">
+                        <label for="mongodb" class="col-3">MongoDB</label>
+                        <input type="checkbox" class="form-control col-3" id="mongodb" name="mongodb">
                     </div>
-                    <div id="test-vl-2" role="tabpanel" class="bs-stepper-pane fade" aria-labelledby="stepper4trigger2">
-                        <div class="form-group">
-                            <label for="exampleInputPasswordV1">
-                                <h2>Set environment variables</h2>
-                            </label>
-                            <textarea rows="10" cols="200" type="text" class="form-control" id="exampleInputPasswordV1">
-CURRENT_THEME=
-MONGO_URI= # if you chose mongodb
-                            </textarea>
-                        </div>
-                        <button class="btn btn-primary" onclick="stepper.previous()">Previous</button>
-                        <button class="btn btn-primary" onclick="stepper.next()">Next</button>
+                    <div class="form-group row">
+                        <label for="mysql" class="col-3">MySQL</label>
+                        <input type="checkbox" class="form-control col-3" id="mysql" name="mysql">
                     </div>
-                    <div id="test-vl-3" role="tabpanel" class="bs-stepper-pane fade" aria-labelledby="stepper4trigger3">
-                        <button class="btn btn-primary mt-5" onclick="stepper.previous()">Previous</button>
-                        <button type="submit" class="btn btn-primary mt-5">Submit</button>
+                    <button class="btn btn-primary" id="next1">Next</button>
+
+                </div>
+                <div id="test-vl-2" role="tabpanel" class="bs-stepper-pane fade" aria-labelledby="stepper4trigger2">
+                    <div class="form-group">
+                        <label for="envvar">
+                            <h2>Set environment variables</h2>
+                        </label>
+                        <textarea rows="10" cols="200" type="text" class="form-control" id="envvar"></textarea>
                     </div>
-                </form>
+                    <button class="btn btn-primary" onclick="stepper.previous()">Previous</button>
+                    <button class="btn btn-primary" onclick="stepper.next()">Next</button>
+                </div>
+                <div id="test-vl-3" role="tabpanel" class="bs-stepper-pane fade" aria-labelledby="stepper4trigger3">
+                    <button class="btn btn-primary mt-5" onclick="stepper.previous()">Previous</button>
+                    <button type="submit" class="btn btn-primary mt-5">Submit</button>
+                </div>
+
             </div>
         </div>
     </div>
 
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bs-stepper/dist/js/bs-stepper.min.js"></script>
 
     <script>
         var stepper;
-        document.addEventListener('DOMContentLoaded', function() {
-            stepper = new Stepper(document.querySelector('#stepper'));
 
+        document.addEventListener("DOMContentLoaded", function() {
+            stepper = new Stepper(document.querySelector('#stepper'));
         })
+
+        $("#next1").click(function() {
+
+
+            const mongodb = $("#mongodb").is(":checked");
+            const mysql = $("#mysql").is(":checked");
+
+            if (mongodb) {
+                $("#envvar").val(`DB_TYPE=mongodb
+MONGO_URI=`)
+            } else if (mysql) {
+                $("#envvar").val(`DB_TYPE=mysql
+DB_HOST=
+DB_PORT=3306
+DB_DATABASE=panda_cms
+DB_USERNAME=
+DB_PASSWORD=
+`)
+            } else {
+                alert("Please select a database");
+                return;
+            }
+
+            stepper.next();
+
+        });
     </script>
 </body>
 
