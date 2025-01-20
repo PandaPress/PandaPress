2b0ae2f93f8d272d9e584a1d136478682cb29ed9
guiyumin <guiyumin@protonmail.com>
2025-01-20 13:49:35 -0800

    fix: scripps executable

--- scripts/d-reload.sh
+++ scripts/d-reload.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [ ! -f compose.yml ]; then
-    echo "\033[31mERROR: compose.yml not found. Run 'make d-setup' first.\033[0m"
+    echo "\033[31mERROR: compose.yml not found. Run 'make d-init' first.\033[0m"
     exit 1
 fi
 
