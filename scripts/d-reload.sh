d205f966edb0aa3caa66a71e41aa248e79e9979c
guiyumin <guiyumin@protonmail.com>
2025-01-22 20:33:47 -0800

    fix: reload dev and prod

--- scripts/d-reload.sh
+++ scripts/d-reload.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
-if [ ! -f compose.development.yml ]; then
-    echo "\033[31mERROR: compose.development.yml not found.\033[0m"
+# Default to development if APP_ENV is not set
+APP_ENV=${APP_ENV:-development}
+COMPOSE_FILE="compose.${APP_ENV}.yml"
+
+if [ ! -f "$COMPOSE_FILE" ]; then
+    echo "\033[31mERROR: ${COMPOSE_FILE} not found.\033[0m"
     exit 1
 fi
 
-
-docker compose -f compose.development.yml restart 
+docker compose -f "$COMPOSE_FILE" restart
