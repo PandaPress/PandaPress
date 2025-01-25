d64000d9bfc2415530dbe4d431518577029989c9
guiyumin <guiyumin@protonmail.com>
2025-01-24 18:32:59 -0800

    fix: d-stop script

--- scripts/d-stop.sh
+++ scripts/d-stop.sh
@@ -6,14 +6,14 @@ APP_ENV=${APP_ENV:-development}
 if [ -z "$APP_ENV" ]; then
 	echo "\033[31mERROR: APP_ENV environment variable is not set.\033[0m"
 	exit 1
-elif [ "$APP_ENV" == "development" ]; then
+elif [ "$APP_ENV" = "development" ]; then
 	if [ ! -f compose.development.yml ]; then
 		echo "\033[31mERROR: compose.development.yml not found.\033[0m"
 		exit 1
 	fi
 
 	docker compose -f compose.development.yml stop
-elif [ "$APP_ENV" == "production" ]; then
+elif [ "$APP_ENV" = "production" ]; then
 	if [ ! -f compose.production.yml ]; then
 		echo "\033[31mERROR: compose.production.yml not found.\033[0m"
 		exit 1
